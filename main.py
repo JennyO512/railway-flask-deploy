fca1a6744fdfd2816bf403ccf8472520774cd367
JLO I/O <99860222+JennyO512@users.noreply.github.com>
2023-06-05 11:57:00 -0500

    Update main.py

--- main.py
+++ main.py
@@ -7,9 +7,29 @@ from picture_api import replicate_api_function
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'super_secret_key'
 
+# Set the upload folder path
+UPLOAD_FOLDER = 'uploaded_images'
+
+# total credits, to be used globally
+#total_credits = 5
+#used_credits = 0
+
 @app.route("/", methods=['GET'])
 def index():
     return render_template('index.html')
+
+uploaded_filename = ""
+
+@app.route('/upload_image', methods=['POST'])
+def upload_image():
+    global uploaded_filename
+    timestamp = datetime.now().strftime('%Y%m%d%H%M%S')
+    image_data = request.json['imageData']
+    filename = f'{timestamp}_uploaded_image.png'
+    uploaded_filename = filename
+    with open(f"uploaded_images/{filename}", 'wb') as f:
+        f.write(base64.b64decode(image_data.split(',')[1]))
+    return jsonify({'message': 'Image uploaded successfully'}), 200
 
 @app.route("/dashboard", methods=['GET', 'POST'])
 def dashboard():
@@ -38,5 +58,9 @@ def dashboard():
 
     return render_template('dashboard.html')
  
+@app.route('/uploads/<filename>')
+def display_image(filename):
+    return send_from_directory(UPLOAD_FOLDER, filename)    
+    
 if __name__ == '__main__':
   app.run(port=5000)
