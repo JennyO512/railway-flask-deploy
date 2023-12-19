4e689fdb56aa49e6dda87e294bb4531ead6ff6aa
Jenny B <99860222+JennyO512@users.noreply.github.com>
2023-12-19 06:22:23 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, jsonify, send_from_directory,url_for, redirect, session
 from flask import flash
+from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
